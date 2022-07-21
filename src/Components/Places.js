22bbe58ad8b66f777370a0f0851cc181e88c407f
ms29ash <ms29ash@gmail.com>
2022-07-21 20:16:21 +0530

    add Api in Places

--- src/Components/Places.js
+++ src/Components/Places.js
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import sc from "styled-components";
+import axios from '../axios'
+
 
 function Places() {
+
+  const [data, setData] = useState();
+
+  const fetchPlaces = async () => {
+    try {
+      const response = await axios.get('/place');
+      console.log(response.data.items);
+      setData(response?.data.items);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPlaces()
+  }, [])
   return (
     <Section>
       <Container>
         <Title>Popular Places</Title>
         <Wrapper>
-          {[
-            { title: "Uttarakhand", bg: "uttarakhand.jpg" },
-            { title: "Assam", bg: "Assam.jpg" },
-            { title: "Laddakh", bg: "laddakh.jpg" },
-            { title: "Meghalaya", bg: "meghalaya.jpg" },
-            { title: "Himanchal", bg: "himanchal.jpg" },
-          ].map((item) => (
-            <Card key={item.title} bg={`./images/${item.bg}`}>
-              <CardWrapper>
-                <Text>{item.title}</Text>
+          {
+            data?.map((item) => {
 
-                <Btn>Learn More</Btn>
-              </CardWrapper>
-            </Card>
-          ))}
+              return <Card key={item._id} bg={`./images/${item.img}`}>
+                <CardWrapper>
+                  <Text>{item.name}</Text>
+
+                  <Btn>Learn More</Btn>
+                </CardWrapper>
+              </Card>
+            })
+          }
         </Wrapper>
       </Container>
     </Section>
@@ -32,69 +47,100 @@ export default Places;
 
 const Section = sc.section`
 width:100vw;
-height:100vh;
+overflow-x:hidden;
+padding:10vh 0;
+height:100%;
 display:grid;
 place-items:center;
-// background:rgba(255, 255, 255, 0.85) url('./test/town.jpg') no-repeat center center/cover;
 background-blend-mode:overlay;
 `;
 
 const Title = sc.h1`
-font-size:2rem;`;
+font-size:2rem;
+text-align:center;
+position:relative;
+padding:0 0 1rem;
+border-bottom:2px solid #22594a;
+&:before { 
+  position:absolute;
+  bottom:0;
+  content:'';
+  right:41%;
+  left:41%;
+  background-color:#22594a;
+  height:7px;
+}
+`;
 
 const Container = sc.div`
 display:flex;
 flex-direction:column;
 width:90%;
-height:80%;
-algin-items:center;
+
 `;
 const Wrapper = sc.div`
-justify-content:space-between;
 align-items:center;
 height:80%;
 margin-top:5%;
 width:100%;
+flex-wrap:wrap;
+justify-content:center;
 display:flex;`;
 
 const Card = sc.div`
 box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-max-width:400px;
-height:100%;
-width:19%;
+max-width:300px;
+min-height:300px;
+margin:0.5rem ;
+height:18vw;
+width:18%;
 color:white;
+align-items:flex-end;
 border-radius:10px;
+display:flex;
 background: ${(props) => `url(${props.bg})`} no-repeat center center/cover;
 transform-origin:center;
 transition: all 150ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
 &:hover{
-transform:scale(1.02);
-background-color:rgb(0,0,0,0.8);
+  background-color:rgb(0,0,0,0.8);
+  div{
+    height:100%;
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    button{
+   
+      display:block;
+    }
+  }
 
 }
+@media only screen and (max-width:1024px) and (min-width:720px){
+  width:30%;
+  height:27vw;
+  }
+@media only screen and (max-width:720px) and (min-width:0px){
+  width:100%;
+  height:90vw;
+  }
 `;
 const CardWrapper = sc.div`
-width:100%;
-height:100%;
+height:30%;
 border-radius:10px;
 display:grid;
 place-items:center;
-&:hover{
-
-  // background-color:#00000070;
-  background-color:#91d7f2ee;
-  backdrop-filter: blur(2px);
-  
-  -webkit-backdrop-filter: blur(2px);
-  backdrop-filter: blur(2px);  
-  button{
-   
-    display:block;
-  }
-}
+backdrop-filter: blur(5px);
+-webkit-backdrop-filter: blur(5px);
+backdrop-filter: blur(5px);
+width:100%;
+transform-origin: bottom;
+  transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
 `;
 const Text = sc.p`
-text-align:center;`;
+text-align:center;
+font-weight:bold;
+
+`;
 const Btn = sc.button`
     padding:0.75rem 3rem;
     border-radius:50px;
@@ -103,4 +149,3 @@ const Btn = sc.button`
     transition: all 150ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
     cursor:pointer;
 `;
-// const Section = sc.section``
