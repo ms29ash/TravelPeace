1a3e03ef996c2e09382ef033b8a957fefe380fc3
ms29ash <ms29ash@gmail.com>
2022-08-29 20:36:12 +0530

    connect form to server

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,43 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import sc from "styled-components";
 import { useForm } from "react-hook-form";
-import axios from '../axios'
-
+import axios from "../axios";
 
 function Form() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState(
+    "Leave us your contact and we'll get you soon"
+  );
+  const [disabled, setDisabled] = useState(false);
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const submitForm = async (data) => {
+    setDisabled(true);
     try {
-      const response = await axios.post('/form/submit', { name: data.name, contact: parseInt(data.contact) });
+      const response = await axios.post("/form/submit", {
+        name: data.name,
+        contact: parseInt(data.contact),
+      });
       console.log(response);
+      if (response.data.success === true) {
+        setModal(true);
+
+        setTimeout(() => {
+          setModal(false);
+          setDisabled(false);
+        }, 8000);
+      } else {
+        setDisabled(false);
+        return;
+      }
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  const onSubmit = data => {
-    console.log(data);
+  const onSubmit = (data) => {
     submitForm(data);
+  };
 
-  }
-
-
-
+  useEffect(() => {
+    reset();
+  }, [modal, reset]);
   return (
     <Section>
       <Container>
-        <FormContainer onSubmit={handleSubmit(onSubmit)} >
+        <FormContainer onSubmit={handleSubmit(onSubmit)}>
           <Title>Say Hello</Title>
-          <Text>Leave us your contact and we'll get you soon</Text>
-          <Input type="text" placeholder="Name"  {...register("name", { required: true, maxLength: 20, minLength: 3 })} />
-          <div>{errors.name && 'Invalid name (minimun 3 letters)'}</div>
-          <Input type="number" placeholder="Contact number"   {...register("contact", { required: true, pattern: /^[0-9]{10}$/i })} />
-          <div>{errors.contact && 'Invalid contact number (10 digits)'}</div>
-          <Button type='submit' >Submit</Button>
+          <Text>{title}</Text>
+          <Input
+            type="text"
+            placeholder="Name"
+            {...register("name", {
+              required: true,
+              maxLength: 20,
+              minLength: 3,
+            })}
+          />
+          <span>{errors.name && "Invalid name (minimun 3 letters)"}</span>
+          <Input
+            type="number"
+            placeholder="Contact number"
+            {...register("contact", {
+              required: true,
+              pattern: /^[0-9]{10}$/i,
+              maxLength: 10,
+              minLength: 10,
+            })}
+          />
+          <span>{errors.contact && "Invalid contact number (10 digits)"}</span>
+          <Button type="submit" disabled={disabled}>
+            Submit
+          </Button>
+          {modal && (
+            <Modal>
+              <h1>Thank you for submission we'll get you soon</h1>
+            </Modal>
+          )}
         </FormContainer>
       </Container>
-    </Section >
+    </Section>
   );
 }
 
@@ -67,8 +114,8 @@ align-items:center;
 width:100%;
 background:white;
 max-width:500px;    
-
-div{
+position:relative;
+span{
   height:8px;
   font-size:0.75rem;
   text-align:left;
@@ -113,6 +160,9 @@ border-radius:50px;
   background-color:#03A66A;
 
 }
+&:disabled{
+  background-color:#03a66a89;
+}
 `;
 
 const Title = sc.h1`
@@ -120,4 +170,24 @@ font-size:2rem;
 `;
 const Text = sc.p`
 margin:1rem 0 2rem;
+`;
+
+const Modal = sc.div`
+position:absolute;
+top:0;
+bottom:0;
+left:0;
+right:0;
+display:grid;
+place-items:center;
+background:url('https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80') no-repeat center center/cover;
+h1{
+  text-align:center;
+  background:#ffffffae;
+  backdrop-filter: blur(10px);
+  color:#000;
+  padding:2rem;
+  z-index:3;
+}
+
 `;
